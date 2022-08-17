03df0fc854bdbe4f116671ca4e2a24805b573b3e
PRVDSP <104918051+PRVDSP@users.noreply.github.com>
2022-08-17 11:48:59 -0300

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,15 @@
+var weight = [10,20,30,40]
+function setup() 
+{
+  createCanvas(400,400);
+  var sum = weight[0] + weight[1] + weight[2] + weight[3]; var average = sum/weight.length; console.log(average);
+
+
+}
+
+function draw() 
+{
+background(51);
+
+}
+
